daf6f4b32e188a39ca11ee8bf9fc22dac35b21d5
TraiLoveLinh <136947709+TraiLoveLinh@users.noreply.github.com>
2025-02-22 20:36:11 +0700

    add curl

--- bro.sh
+++ bro.sh
@@ -1,4 +1,4 @@
-apk add neovim git build-base wget
+apk add neovim git build-base wget curl
 git clone https://github.com/TraiLoveLinh/nvim.git ~/.config/nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
